c15dcaaec5ea295529ffb9122c58a55a3732782c
narjirolu125 <narjirolu@gmail.com>
2020-07-05 19:39:40 +0700

    lavender: update defcofig path
    Signed-off-by: narjirolu125 <narjirolu@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,7 +43,7 @@ BOARD_KERNEL_CMDLINE += skip_initramfs rootwait ro init=/init
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/xiaomi/lavender
-TARGET_KERNEL_CONFIG := lavender-perf_defconfig
+TARGET_KERNEL_CONFIG := lavender_defconfig
 
 # Manifest
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
